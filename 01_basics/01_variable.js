768eee5080d86a1857cc1babcf5ca84e50484cb4
Anish Chettri <113011778+Anish770@users.noreply.github.com>
2024-04-02 12:02:01 +0000

    done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId=34243
+var accountPassword="zen" 
+let accountEmail="anishchettri7@gmail.com"
+accountCity="Sikkim"
+let accountStatus;
+
+// accountId=2 // not allowed, only const cannot be changed
+accountPassword="okzen"// works
+accountEmail="zenxd123@gmail.com"
+accountCity="Kolkata"
+// console.log(accountCity);
+console.table([accountId,accountPassword,accountEmail,accountCity,accountStatus])
