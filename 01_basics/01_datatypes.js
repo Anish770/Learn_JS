55957e79c3109a875736a8a0234ae362c125ee63
Anish Chettri <113011778+Anish770@users.noreply.github.com>
2024-04-02 12:30:44 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all JS codes as newer version
+// alert(3+3)//works only in browser not in node js
+console.log(3+3)
+let name="anish"
+let age=21
+let isLoggedIn=false;
+
+//Number=> range of numbers are 2^52 or 2^53
+// BigInt=> for larger than number
+// String=>""
+// Boolean=>true/false
+// null=> standalone value or the representation of null value
+// undefined=> is when the value is not assigned to it
+// Symbol=>to find the uniqueness
+
+// Object
+console.log(typeof(name))//string
+console.log(typeof(age))//number
+console.log(typeof(isLoggedIn))//boolean
+console.log(typeof(null))//object
+console.log(typeof(undefined))//undefined
