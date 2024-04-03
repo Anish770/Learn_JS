fa3edad9b03bac79afe07370b023f0b3403b9c16
Anish Chettri <113011778+Anish770@users.noreply.github.com>
2024-04-03 08:08:58 +0000

    updated

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,7 @@
+// console.log("02">1)
+console.log(null==0)
+console.log(null>=0)
+console.log(null<=0)
+// console.log(+null)
+console.log("2"===2)//checks strictly it checks both the value and it's dataype
+// console.log(null===0)
