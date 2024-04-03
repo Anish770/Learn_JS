f046e958fa9d4a2e927b4a591f05e98baa9ae03d
Anish Chettri <113011778+Anish770@users.noreply.github.com>
2024-04-03 08:19:34 +0000

    summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,10 @@
+const sample1=Symbol("123")
+const sample2=Symbol("123")
+console.log(typeof sample1)
+console.log(typeof sample2)
+
+const myfunc=function()
+{
+    return 2
+}
+console.log(myfunc)
