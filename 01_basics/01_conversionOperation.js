ad97f0ee2492224e7a0e059fa022e0e18fa2e9ed
Anish Chettri <113011778+Anish770@users.noreply.github.com>
2024-04-03 07:21:03 +0000

    Conversion of Datatypes

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,16 @@
+let score=true
+// console.log(typeof(score))
+// console.log(score)
+// let numberinValue=Number(score)
+// console.log(typeof(numberinValue))
+// console.log(numberinValue)
+// //"33"=>33
+//"33ab"=> NaN
+//true =>1
+let isLoggedIn="anish"
+console.log(isLoggedIn)
+let boolvar=Boolean(isLoggedIn)
+console.log(typeof(boolvar))
+console.log(boolvar)
+//1=>true;0=>false
+//""=>false;"anish"=>true
